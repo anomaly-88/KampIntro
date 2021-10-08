9a85911f3f28b67db71713b59ec77d14022b95fd
Root <alfahesap@gmail.com>
2021-10-08 23:05:10 +0300

    Konu yazıldı.

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -6,8 +6,23 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Github denemesi");
+            // Primative tip olan int bir değer tiptir. Primative tipe sahip değişkenler değerleri tutarlar. (int, str, bool, double, float..)
+            int sayi1 = 30;
+            int sayi2 = 60;
+            sayi1 = sayi2;
+            sayi2 = 99;
+            Console.WriteLine(sayi1);
+
+
+            // Array bir referans tiptir. Referans tipe sahip değişkenler adresleri tutarlar. (Class, Abstract Class, Array, Interface..)
+            int[] sayilar1 = new int[] { 1,3,5,7,9 };
+            int[] sayilar2 = new int[] { 2, 4, 6, 8 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            foreach (int sayi in sayilar1)
+            {
+                Console.Write(sayi+" ");
+            }
         }
     }
 }
