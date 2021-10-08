2b47b2ecd0f909c9dcc12d242a7afc5310ede817
Root <alfahesap@gmail.com>
2021-10-08 22:33:00 +0300

    test

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -7,6 +7,7 @@ namespace DegerVeReferansTipler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Github denemesi");
         }
     }
 }
